33fce95fde5692736b6b5aabd9b9d9667eb1ab6d
jan-klos <janklos@protonmail.com>
2018-04-15 00:30:11 +0200

    output file name changed

--- ytmp3.sh
+++ ytmp3.sh
@@ -3,5 +3,5 @@
 file=$1
 while IFS= read -r link
 do
-	youtube-dl --extract-audio --audio-format mp3 $link
+	youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $link
 done < "$file"
